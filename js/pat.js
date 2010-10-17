fbf497641c87c48a9d8ca03a6967e8e134713dae
Greg Heartsfield <scsibug@imap.cc>
2010-10-17 18:46:08 -0500

    Brought 'things' functionality up to par with 'places.

--- js/pat.js
+++ js/pat.js
@@ -1,7 +1,11 @@
 var place_prefix = "pat.p."
+var thing_prefix = "pat.t."
 
 function addPlace(name) {
   window.localStorage[place_prefix+name] = name
+}
+function addThing(name) {
+  window.localStorage[thing_prefix+name] = name
 }
 
 function debugPlaces() {
@@ -35,4 +39,30 @@ function getPlaces() {
     }
   }
   return places
+}
+
+function displayThings() {
+  var p = getThings();
+  $('#display-things-ulist').remove();
+  var list = $("<ul>", {"class": "ui-listbox-list",
+                        "id": "display-things-ulist",
+                        "data-inset": "true"}
+              ).appendTo('#thingsIntro');
+  $.each(p,function(k,v) {
+    $('<li><a href="#'+k+'">'+k+'</a></li>').appendTo(list);
+  });
+  list.listview();
+}
+
+function getThings() {
+  // things are identified as keys with prefix "pat.t."
+  var things = new Object();
+  keycount = window.localStorage.length;
+  for(i=0;i<keycount;i++) {
+    key = localStorage.key(i);
+    if (key.substr(0,6) == thing_prefix) {
+      things[key.substr(6)] = window.localStorage.getItem(key)
+    }
+  }
+  return things
 }
